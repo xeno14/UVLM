c187899908251800b8d3b019d4dd9b9e5ac5eace
Ryo Murakami <integral14@gmail.com>
2015-05-28 15:29:19 +0900

    flap test

--- test/morphing_test.cpp
+++ test/morphing_test.cpp
@@ -22,10 +22,26 @@ class MorphingVelocityTest : public ::testing::Test {
 TEST_F(MorphingVelocityTest, plug) {
   morphing.set_plug([](double t) { return sin(t); });
   for (double t=0; t<M_PI; t+=0.1) {
-    Eigen::Vector3d v;
-    morphing.Velocity(&v, span[0], t);
-    EXPECT_DOUBLE_EQ(0, v.x());
-    EXPECT_DOUBLE_EQ(0, v.y());
-    EXPECT_NEAR(cos(t), v.z(), EPS);
+    for (const auto& x0 : span) {
+      Eigen::Vector3d v;
+      morphing.Velocity(&v, x0, t);
+      EXPECT_DOUBLE_EQ(0, v.x());
+      EXPECT_DOUBLE_EQ(0, v.y());
+      EXPECT_NEAR(cos(t), v.z(), EPS);
+    }
+  }
+}
+
+TEST_F(MorphingVelocityTest, flap) {
+  auto phi = [](double t) { return sin(t); };
+  morphing.set_flap(phi);
+  for (double t = 0; t < M_PI; t += 0.1) {
+    for (const auto& x0 : span) {
+      Eigen::Vector3d v;
+      morphing.Velocity(&v, x0, t);
+      EXPECT_DOUBLE_EQ(0, v.x());
+      EXPECT_NEAR(-x0.y() * sin(phi(t)) * cos(t), v.y(), EPS);
+      EXPECT_NEAR( x0.y() * cos(phi(t)) * cos(t), v.y(), EPS);
+    }
   }
 }
