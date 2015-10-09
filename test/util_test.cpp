ef5bd5a462ff8c092192ab6815557ce3f3f29e3e
Ryo Murakami <integral14@gmail.com>
2015-10-10 00:45:06 +0900

    chebyshev

--- test/util_test.cpp
+++ test/util_test.cpp
@@ -2,9 +2,16 @@
 
 #include "util.h"
 
-TEST(linspaceTest, three_points) {
+TEST(linspaceTest, plus) {
   auto x = linspace(-1, 1, 3);
   EXPECT_DOUBLE_EQ(-1, x[0]);
   EXPECT_DOUBLE_EQ(0, x[1]);
   EXPECT_DOUBLE_EQ(1, x[2]);
+}
+
+TEST(linspaceTest, minus) {
+  auto x = linspace(1, -1, 3);
+  EXPECT_DOUBLE_EQ(1, x[0]);
+  EXPECT_DOUBLE_EQ(0, x[1]);
+  EXPECT_DOUBLE_EQ(-1, x[2]);
 }
