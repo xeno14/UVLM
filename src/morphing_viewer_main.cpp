7e2f328cd29d3952b330a87d2e65bacc8fedcb89
Ryo Murakami <integral14@gmail.com>
2015-10-12 12:37:41 +0900

    no twist

--- src/morphing_viewer_main.cpp
+++ src/morphing_viewer_main.cpp
@@ -11,8 +11,8 @@ int main(int argc, char* argv[]) {
   set_plug([](double t) { return sin(t); });
   set_flap([](double t) { return M_PI/6 * sin(t); });
   set_alpha(0);
-  set_twist([](const Eigen::Vector3d& x0, double t) {
-    return fabs(x0.y()) * sin(t);
-  });
+  // set_twist([](const Eigen::Vector3d& x0, double t) {
+  //   return fabs(x0.y()) * sin(t);
+  // });
   return morphing_viewer::main(argc, argv);
 }
