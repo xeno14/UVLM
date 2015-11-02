52f759ac9b2c4553c0b4e9e1653e7af7e3565ee7
Ryo Murakami <integral14@gmail.com>
2015-11-02 17:57:05 +0900

    remove unused funcs

--- src/shed.h
+++ src/shed.h
@@ -11,77 +11,14 @@
 namespace UVLM {
 namespace internal {
 
-/** @brief Euler-methodで移流させる
- *  @param[out] target 位置
- *  @param[in]  vel 速度
- *  @param[in]  dt  時間刻み
- */
-inline void Advect(Eigen::Vector3d* const target, const Eigen::Vector3d& vel,
-            const double dt) {
-  *target = *target + vel * dt;
-}
-
-/** @biref Trailing edgeの渦を一つ放出する
- *  @param[out] result 放出された渦
- *  @param[in]  target 放出される前のtrailing edgeにある渦
- *  @param[in]  rings 渦
- *  @param[in]  dt 時間刻み
- */
-void ShedSingleAtTrailingEdge(VortexRing* result, const VortexRing& target,
-                              const UVLMVortexRing& rings,
-                              const Eigen::Vector3d& Vinfty, const double t,
-                              const double dt);
-
-/** @brief 移流の実装
- *
- *  result に移流した結果を入れる
- *  @todo ここが律速になるはずなので並列化する
- *  @param[out] result 移流の結果
- *  @param[in]  rings 渦
- *  @param[in]  dt 時間刻み
- */
-void AdvectWakeImpl(std::vector<VortexRing>* result,
-                    const UVLMVortexRing& rings, const Eigen::Vector3d& Vinfty,
-                    const double dt);
-
 template <class InputIterator>
 void AdvectKernel(VortexRing* v, InputIterator first, InputIterator last,
     const Eigen::Vector3d& freestream, const double dt);
 
-template <class InputIterator>
-void AdvectWakeImpl(std::vector<UVLM::VortexRing>* wake,
-                    InputIterator vortices_first, InputIterator vortices_last,
-                    const Eigen::Vector3d& Vinfty, const double dt);
-
 }  // namespace internal
-
-template <class InputIterator, class OutputIterator>
-void ShedAtTrailingEdge(InputIterator first, InputIterator last,
-                        OutputIterator result, const UVLMVortexRing& rings,
-                        const Eigen::Vector3d& Vinfty, const double t, const double dt) {
-  while (first != last) {
-    internal::ShedSingleAtTrailingEdge(&(*result), *first, rings, Vinfty, t, dt);
-    ++first; ++result;
-  }
-}
-
-template <class InputIterator1, class InputIterator2, class OutputIterator>
-void ShedAtTrailingEdge(InputIterator1 edge_first, InputIterator1 edge_last,
-                        OutputIterator result, InputIterator2 vortices_first,
-                        InputIterator2 vortices_last,
-                        const UVLMVortexRing& rings,
-                        const Eigen::Vector3d& Vinfty, const double t,
-                        const double dt);
 
 template <class OutputIterator>
 void ConnectTrailingEdge(OutputIterator wake, const VortexContainer& c);
-
-template <class InputIterator, class OutputIterator>
-void AttachShedVorticesToEdge(InputIterator edge_first, InputIterator edge_last,
-                              OutputIterator result);
-
-void AdvectWake(UVLMVortexRing* rings, const Eigen::Vector3d& Vinfty,
-                const double dt);
 
 template <class InputIterator, class OutputIterator>
 void Advect(InputIterator vortex_first, InputIterator vortex_last,
@@ -92,12 +29,6 @@ template <class InputIterator, class OutputIterator>
 void AdvectParallel(InputIterator vortex_first, InputIterator vortex_last,
                     OutputIterator wake_first, OutputIterator wake_last,
                     const Eigen::Vector3d& freestream, const double dt);
-
-template <class InputIterator, class OutputIterator>
-void AdvectWake(OutputIterator wake_first, OutputIterator wake_last,
-                InputIterator vortices_first, InputIterator vortices_last,
-                const UVLMVortexRing& rings,
-                const Eigen::Vector3d& Vinfty, const double dt);
 
 template <class InputIterator>
 void InducedVelocity(Eigen::Vector3d* const result,
--- src/shed.inl
+++ src/shed.inl
@@ -5,20 +5,6 @@
 namespace UVLM {
 namespace internal {
 
-template <class InputIterator, class OutputIterator>
-void AdvectWakeImpl(OutputIterator wake_first, OutputIterator wake_last,
-                    InputIterator vortices_first, InputIterator vortices_last,
-                    const Eigen::Vector3d& Vinfty, const double dt) {
-  for (auto wake = wake_first; wake != wake_last; ++wake) {
-    Eigen::Vector3d velocity;
-    for (auto& node : wake->nodes()) {
-      InducedVelocity(&velocity, node, vortices_first, vortices_last);
-      velocity += Vinfty;
-      internal::Advect(&node, velocity, dt);
-    }
-  }
-}
-
 template <class InputIterator>
 void AdvectKernel(VortexRing* v, InputIterator first, InputIterator last,
     const Eigen::Vector3d& freestream, const double dt) {
@@ -32,17 +18,6 @@ void AdvectKernel(VortexRing* v, InputIterator first, InputIterator last,
 }
 
 }  // namespace internal
-
-template <class InputIterator>
-void AdvectWake(std::vector<UVLM::VortexRing>* wake,
-                InputIterator vortices_first, InputIterator vortices_last,
-                const UVLMVortexRing& rings,
-                const Eigen::Vector3d& Vinfty, const double dt) {
-  // TODO こいつが悪い
-  // internal::AdvectWakeImpl(new_wake.begin(), new_wake.end(), vortices_first,
-  //                          vortices_last, Vinfty, dt);
-  internal::AdvectWakeImpl(wake, rings, Vinfty, dt);
-}
 
 template <class InputIterator>
 void InducedVelocity(Eigen::Vector3d* result,
@@ -93,45 +68,12 @@ void ChordwiseInducedVelocity(Eigen::Vector3d* const result,
   }
 }
 
-template <class InputIterator1, class InputIterator2, class OutputIterator>
-void ShedAtTrailingEdge(InputIterator1 edge_first, InputIterator1 edge_last,
-                        OutputIterator result, InputIterator2 vortices_first,
-                        InputIterator2 vortices_last,
-                        const UVLMVortexRing& rings,
-                        const Eigen::Vector3d& Vinfty, const double t,
-                        const double dt) {
-  for (auto edge = edge_first; edge != edge_last; ++edge, ++result) {
-    *result = *edge;
-    Eigen::Vector3d velocity;
-    // 移流する
-    for (auto& node : result->nodes()) {
-      // TODO ここがおかしい
-      // InducedVelocity(&velocity, node, vortices_first, vortices_last);
-      // rings.InducedVelocity(&velocity, node);
-
-      // velocity = Vinfty;
-      velocity = Eigen::Vector3d(1, 0, 0);
-      internal::Advect(&node, velocity, dt);
-    }
-  }
-}
-
 template <class InputIterator, class OutputIterator>
 void ConnectTrailingEdge(InputIterator edge_first, InputIterator edge_last,
                          OutputIterator wake) {
   for (auto edge = edge_first; edge != edge_last; ++edge, ++wake) {
     wake->nodes()[0] = edge->nodes()[1];
     wake->nodes()[3] = edge->nodes()[2];
-  }
-}
-
-template <class InputIterator, class OutputIterator>
-void AttachShedVorticesToEdge(InputIterator edge_first, InputIterator edge_last,
-                              OutputIterator result) {
-  while (edge_first != edge_last) {
-    result->nodes()[0] = edge_first->nodes()[1];
-    result->nodes()[3] = edge_first->nodes()[2];
-    ++edge_first; ++result;
   }
 }
 
