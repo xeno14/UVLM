62fbd963f3a49df23b47de154245ed9ab365909d
Ryo Murakami <integral14@gmail.com>
2015-11-02 14:31:39 +0900

    linear test
    linear test

--- test/linear_test.cpp
+++ test/linear_test.cpp
@@ -9,7 +9,7 @@ using UVLM::Morphing;
 using UVLM::VortexRing;
 
 namespace {
-const double EPS = 1e-6;
+const double EPS = 1e-10;
 }
 
 class CalcMorphingVelocityTest : public ::testing::Test {
@@ -46,4 +46,72 @@ TEST_F(CalcMorphingVelocityTest, flap) {
   EXPECT_NEAR(-2, result[1], EPS);
 
   // TODO test twist
+}
+
+class CalcSolveLinearTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    vortices = std::vector<VortexRing>{GetSquareRing(2, -1, 0),
+                                       GetSquareRing(2, 1, 0)};
+    for (auto& v : vortices) v.SaveReferenceNode();
+  }
+  std::vector<VortexRing> vortices;
+};
+
+TEST_F(CalcSolveLinearTest, no_wake_no_morphing) {
+  std::vector<VortexRing> dummy;
+  Morphing m;
+  auto result = UVLM::SolveLinearProblem(vortices.cbegin(), vortices.cend(),
+                                         dummy.cbegin(), dummy.cend(),
+                                         Eigen::Vector3d(0, 0, 1), m, 0);
+  vortices[0].set_gamma(result(0));
+  vortices[1].set_gamma(result(1));
+  Eigen::Vector3d v0, v1;
+  UVLM::InducedVelocity(&v0, Eigen::Vector3d(-1, 0, 0), vortices.cbegin(),
+                        vortices.cend());
+  UVLM::InducedVelocity(&v1, Eigen::Vector3d(1, 0, 0), vortices.cbegin(),
+                        vortices.cend());
+  EXPECT_VECTOR3D_EQ(0, 0, -1, v0);
+  EXPECT_VECTOR3D_EQ(0, 0, -1, v1);
+}
+
+TEST_F(CalcSolveLinearTest, no_wake_morphing) {
+  std::vector<VortexRing> dummy;
+  Morphing m;
+  m.set_plug([](double t) { return 0.1 * sin(t); });
+  auto result = UVLM::SolveLinearProblem(vortices.cbegin(), vortices.cend(),
+                                         dummy.cbegin(), dummy.cend(),
+                                         Eigen::Vector3d(0, 0, 1), m, 0);
+  vortices[0].set_gamma(result(0));
+  vortices[1].set_gamma(result(1));
+  Eigen::Vector3d v0, v1;
+  UVLM::InducedVelocity(&v0, Eigen::Vector3d(-1, 0, 0), vortices.cbegin(),
+                        vortices.cend());
+  UVLM::InducedVelocity(&v1, Eigen::Vector3d(1, 0, 0), vortices.cbegin(),
+                        vortices.cend());
+  EXPECT_VECTOR3D_NEAR(0, 0, -1 + 0.1, v0, EPS);
+  EXPECT_VECTOR3D_NEAR(0, 0, -1 + 0.1, v1, EPS);
+}
+
+TEST_F(CalcSolveLinearTest, wake_morphing) {
+  std::vector<VortexRing> wake{GetSquareRing(2, 2, 0)};
+  Morphing m;
+  m.set_plug([](double t) { return 0.1 * sin(t); });
+  auto result = UVLM::SolveLinearProblem(vortices.cbegin(), vortices.cend(),
+                                         wake.cbegin(), wake.cend(),
+                                         Eigen::Vector3d(0, 0, 1), m, 0);
+  vortices[0].set_gamma(result(0));
+  vortices[1].set_gamma(result(1));
+  Eigen::Vector3d v0, v1;
+  UVLM::InducedVelocity(&v0, Eigen::Vector3d(-1, 0, 0), vortices.cbegin(),
+                        vortices.cend());
+  UVLM::InducedVelocity(&v1, Eigen::Vector3d(1, 0, 0), vortices.cbegin(),
+                        vortices.cend());
+  Eigen::Vector3d w0, w1;
+  UVLM::InducedVelocity(&w0, Eigen::Vector3d(-1, 0, 0), wake.cbegin(),
+                        wake.cend());
+  UVLM::InducedVelocity(&w1, Eigen::Vector3d(1, 0, 0), wake.cbegin(),
+                        wake.cend());
+  EXPECT_VECTOR3D_NEAR(0, 0, -1 + 0.1 + w0.z(), v0, EPS);
+  EXPECT_VECTOR3D_NEAR(0, 0, -1 + 0.1 + w1.z(), v1, EPS);
 }
