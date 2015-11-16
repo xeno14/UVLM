0aa6622ddfad5d399289b835ada4f9049811fac0
Ryo Murakami <integral14@gmail.com>
2015-11-16 18:11:42 +0900

    origin

--- src/wing/wing.h
+++ src/wing/wing.h
@@ -40,24 +40,26 @@ inline void WholeWing(::UVLM::proto::Wing* wing,
 
   const std::size_t COLS = half.cols() + 1;
   const std::size_t ROWS = half.points().size() / COLS;
-  std::Vector3d origin = PointToVector3d(half.origin());
+  const Eigen::Vector3d origin = PointToVector3d(half.origin());
 
   for (std::size_t i = 0; i < ROWS; ++i) {
     std::vector<Eigen::Vector3d> row_points(
         half_points.begin() + i * COLS, half_points.begin() + (i + 1) * COLS);
     std::vector<Eigen::Vector3d> mirror_row_points(COLS);
     std::transform(row_points.begin(), row_points.end(),
-                   mirror_row_points.begin(),
-                   [origin](const Eigen::Vector3d& p) {
-                     Eigen::Vector3d q = p - origin;
-                     q.y() *= -1;
-                     return origin + q;
+                   mirror_row_points.begin(), [](const Eigen::Vector3d& p) {
+                     Eigen::Vector3d res(p);
+                     res.y() *= -1;
+                     return res;
                    });
     whole_points.insert(whole_points.end(), mirror_row_points.rbegin(),
                         mirror_row_points.rend());
     whole_points.insert(whole_points.end(), row_points.begin() + 1,
                         row_points.end());
   }
+  // move back origin
+  for (auto& p : whole_points) p += origin;
+
   for (const auto& p : whole_points) {
     wing->add_points()->CopyFrom(Vector3dToPoint(p));
   }
