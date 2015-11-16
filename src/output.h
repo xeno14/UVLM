0b069ac3c8ca0425450d50bce255ce1a4c5c22d3
Ryo Murakami <integral14@gmail.com>
2015-11-16 14:46:04 +0900

    iterator value type check

--- src/output.h
+++ src/output.h
@@ -24,8 +24,9 @@ void SimpleAppendSnapshot(UVLM::proto::Snapshot2* snapshot,
                       InputIteraotr1 pos_first, InputIteraotr1 pos_last,
                       InputIteraotr2 gamma_first, InputIteraotr2 gamma_last,
                       const std::size_t cols) {
-  // static_assert(typename std::iterator_traits<InputIteraotr1>::value_type
-  // TODO size check
+  ITERATOR_VALUETYPE_CHECK(Eigen::Vector3d, InputIteraotr1);
+  ITERATOR_VALUETYPE_CHECK(double, InputIteraotr2);
+
   const std::size_t pos_size = std::distance(pos_first, pos_last);
   const std::size_t gamma_size = std::distance(gamma_first, gamma_last);
   CHECK(pos_size == (cols + 1) * (gamma_size / cols + 1));
