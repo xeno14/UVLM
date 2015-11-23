1daeefe36efcf11ba3f2f2bb42363084707235e1
Ryo Murakami <integral14@gmail.com>
2015-11-24 00:54:06 +0900

    use functions

--- src/result_to_vtk.cpp
+++ src/result_to_vtk.cpp
@@ -8,6 +8,7 @@
 #include "vtk/vtk.h"
 
 #include <gflags/gflags.h>
+#include <glog/logging.h>
 #include <cstdio>
 #include <chrono>
 #include <fstream>
@@ -16,26 +17,23 @@
 
 
 DEFINE_string(input, "", "input files with format. e.g. \%08d.dat");
-DEFINE_string(output, "stdout", "output destination [stdout, path to file]");
+DEFINE_string(output, "", "output destination");
 
 using namespace UVLM;
 
 int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
   gflags::ParseCommandLineFlags(&argc, &argv, true);
+  FLAGS_logtostderr = true;
   
-  FILE* fp = nullptr;
-  if (FLAGS_output == "stdout") {
-    fp = stdout;
-  } else {
-    fp = fopen(FLAGS_output.c_str(), "w");
-  }
-  CHECK_OPEN(fp);
-  std::cerr << "Output: " << FLAGS_output << std::endl;
+  FILE* fp = fopen(FLAGS_output.c_str(), "w");
+  CHECK(fp);
 
   std::ifstream ifs(FLAGS_input, std::ios::binary);
-  CHECK_OPEN(ifs);
-  std::cerr << "Input : " << FLAGS_input << std::endl;
+  CHECK(ifs);
 
+  LOG(INFO) << FLAGS_input << " --> " << FLAGS_output;
   proto::Snapshot2 snapshot;
   snapshot.ParseFromIstream(&ifs);
   UVLM::vtk::WriteSnapshot2(fp, snapshot);
