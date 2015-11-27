9411442d37fff59998bceb11f5db2b48f085ecd9
Ryo Murakami <integral14@gmail.com>
2015-11-27 18:15:32 +0900

    cutoff test

--- test/vortex_kernel_test.cpp
+++ test/vortex_kernel_test.cpp
@@ -5,18 +5,25 @@
 
 using UVLM::vortex_kernel::CutOffKernel;
 
-class CutOffTest : public ::testing::Test {};
+const double EPS = 1e-10;
+
+class CutOffTest : public ::testing::Test {
+ protected:
+  CutOffTest() : kernel(1e-5) {}
+  CutOffKernel kernel;
+};
 
 TEST_F(CutOffTest, cutoff) {
-  CutOffKernel kernel(1e-6);
-  auto v = kernel.Induce(
-      {0, 0, 0},
-      {0, 0, -1},
-      {0, 0, 1}, 1);
+  auto v = kernel.Induce({0, 0, 0}, {0, 0, -1}, {0, 0, 1}, 1);
   EXPECT_VECTOR3D_EQ(0, 0, 0, v);
 }
 
 TEST_F(CutOffTest, infinite_length) {
   // Sufficiently long line segment induces velocity of G /2 pi r
-  CutOffKernel kernel(1e-6);
+
+  const double r = 3;
+  const double g = 2;
+  EXPECT_VECTOR3D_NEAR(0, g / 2 / M_PI / r, 0,
+                       kernel.Induce({r, 0, 0}, {0, 0, -1e10}, {0, 0, 1e10}, g),
+                       EPS);
 }
