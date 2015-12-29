591c51bbe6be6e51823d0233e0590352fa3b8e01
Ryo Murakami <integral14@gmail.com>
2015-12-29 18:02:23 +0900

    velocity at

---
+++ src/tools/velocity_at_main.cpp
@@ -0,0 +1,74 @@
+/**
+ * @file velocity_at_main.cpp
+ * @brief Add description here
+ */
+
+
+#include "../proto_adaptor.h"
+#include "../shed.h"
+#include "../util.h"
+#include "../recordio/recordio_range.h"
+
+#include <cstdio>
+#include <fstream>
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <vector>
+#include <map>
+#include <set>
+
+
+DEFINE_string(input, "", "snapshot2 recordio");
+DEFINE_string(output, "", "output");
+DEFINE_double(x, 0, "x");
+DEFINE_double(y, 0, "y");
+DEFINE_double(z, 0, "z");
+
+namespace {
+
+struct Data {
+  double t;
+  Eigen::Vector3d v;
+};
+
+Data Calc(const UVLM::proto::Snapshot2& snapshot) {
+  const double t = snapshot.t();
+
+  std::vector<UVLM::VortexContainer> containers;
+  auto vortices = UVLM::Snapshot2ToContainers(&containers, snapshot);
+
+  const Eigen::Vector3d pos{FLAGS_x, FLAGS_y, FLAGS_z};
+  Eigen::Vector3d v;
+  UVLM::InducedVelocity(&v, pos, vortices->begin(), vortices->end());
+
+  return Data{t, v};
+}
+
+
+}  // anonymous namespace
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InstallFailureSignalHandler();
+  FLAGS_logtostderr = true;
+
+  std::ofstream ofs(FLAGS_output);
+  CHECK(ofs);
+
+  std::size_t count = 0;
+  for (const auto& snapshot :
+       recordio::ReaderRange<UVLM::proto::Snapshot2>(FLAGS_input)) {
+    auto data = Calc(snapshot);
+
+    ofs << data.t << "\t" 
+        << data.v.x() << "\t"
+        << data.v.y() << "\t"
+        << data.v.z() << "\n";
+    ++count;
+  }
+  LOG(INFO) << count << " lines are written."
+            << (count == 0 ? " Is input file correct?" : "");
+
+  return 0;
+}
