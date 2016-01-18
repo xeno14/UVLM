08f8080890f2bfaeb80aa9e66410d76a355e7444
Ryo Murakami <integral14@gmail.com>
2016-01-19 00:12:48 +0900

    3d velocity

--- src/tools/3d_velocity_main.cpp
+++ src/tools/3d_velocity_main.cpp
@@ -25,28 +25,30 @@ DEFINE_string(output, "", "output filename");
 DEFINE_string(range, "-1,-1,1,1", "min0,max0,min1,max1");
 DEFINE_double(at, 0., "position of plane.");
 DEFINE_string(prefix, "v", "prefix of output files");
-DEFINE_int32(step, 0, "step");
+DEFINE_uint64(step, 0, "step");
 
 namespace {
 
-auto CreatePoints(const double xmin, const double xmax, const std::size_t Nx;
-                  const double ymin, const double ymax, const std::size_t Ny;
-                  const double zmin, const double zmax, const std::size_t Nz;) {
+auto CreatePoints(const double xmin, const double xmax, const std::size_t Nx,
+                  const double ymin, const double ymax, const std::size_t Ny,
+                  const double zmin, const double zmax, const std::size_t Nz) {
   const auto xs = linspace(xmin, xmax, Nx);
   const auto ys = linspace(ymin, ymax, Ny);
   const auto zs = linspace(zmin, zmax, Nz);
 
+  std::vector<Eigen::Vector3d> res;
   for (double z : zs) {
     for (double y : ys) {
       for (double x : xs) {
-        points.emplace_back(x, y, z);
+        res.emplace_back(x, y, z);
       }
     }
   }
-  return points;
+  return res;
 }
 
-std::vector<UVLM::VortexRing> GetVortices(const UVLM::proto::Snapshot2& snapshot) {
+std::vector<UVLM::VortexRing> GetVortices(
+    const UVLM::proto::Snapshot2& snapshot) {
   std::vector<UVLM::VortexRing> res;
   res.resize(snapshot.vortices().size());
   std::transform(snapshot.vortices().begin(), snapshot.vortices().end(),
@@ -61,9 +63,8 @@ struct Data {
 };
 
 template <class InputIterator1, class InputIterator2>
-auto CalcData(InputIterator1 pos_first,
-              InputIterator1 pos_last, InputIterator2 v_first,
-              InputIterator2 v_last) {
+auto CalcData(InputIterator1 pos_first, InputIterator1 pos_last,
+              InputIterator2 v_first, InputIterator2 v_last) {
   std::vector<Data> data;
   const std::size_t size = std::distance(pos_first, pos_last);
   data.resize(size);
@@ -79,52 +80,64 @@ auto CalcData(InputIterator1 pos_first,
   return data;
 }
 
+std::size_t Write(const UVLM::proto::Snapshot2& snapshot,
+                  const std::string& output) {
+  const std::size_t Nx = 10;
+  const std::size_t Ny = 10;
+  const std::size_t Nz = 10;
+  const double xmin = 0;
+  const double xmax = 8;
+  const double ymin = -4;
+  const double ymax = 4;
+  const double zmin = -4;
+  const double zmax = 4;
+  const double dx = (xmax - xmin) / (Nx - 1);
+  const double dy = (ymax - ymin) / (Ny - 1);
+  const double dz = (zmax - zmin) / (Nz - 1);
 
-std::size_t Output(const std::vector<Data>& data, const std::string& output) {
-  std::ofstream ofs(output);
-  CHECK(ofs) << "Unable to open " << output;
-  LOG(INFO) << "Output: " << output;
-  const std::string sep = Sep();
-  ofs << "x" << sep
-      << "y" << sep
-      << "z" << sep
-      << "u" << sep
-      << "v" << sep
-      << "w" << std::endl;
-  for (const auto& d : data) {
-    ofs << d.pos.x() << sep
-        << d.pos.y() << sep
-        << d.pos.z() << sep
-        << d.vel.x() << sep
-        << d.vel.y() << sep
-        << d.vel.z() << std::endl;
-  }
-}
+  LOG(INFO) << "[" << xmin << "," << xmax << "]"
+            << "x"
+            << "[" << ymin << "," << ymax << "]"
+            << "x"
+            << "[" << zmin << "," << zmax << "]"
+            << " dimensions "
+            << "(" << Nx << "," << Ny << "," << Nz << ")";
 
-std::size_t Write(const UVLM::proto::Snapshot2& snapshot, const std::string& output) {
-  const auto points = CreatePoints();
+  const auto points =
+      CreatePoints(xmin, xmax, Nx, ymin, ymax, Ny, zmin, zmax, Nz);
   const auto vortices = GetVortices(snapshot);
-  const auto data = CalcData(points.cbegin(), points.cend(), vortices.cbegin(),
-                             vortices.cend());
-  return Output(data, output);
-}
+  const auto result = CalcData(points.cbegin(), points.cend(),
+                               vortices.cbegin(), vortices.cend());
 
-std::string OutputPath(const std::string& output_path,
-                       const std::size_t index) {
-  char path[256];
-  sprintf(path, "%s/%s%08lu.%s", output_path.c_str(), FLAGS_prefix.c_str(),
-          index, FLAGS_filetype.c_str());
-  return std::string(path);
+  FILE* fp = fopen(output.c_str(), "w");
+  CHECK(fp != NULL) << "Unable to open " << output;
+
+  fprintf(fp, "# vtk DataFile Version 2.0\n");
+  fprintf(fp, "HOGE FLOW\n");
+  fprintf(fp, "ASCII\n");
+  fprintf(fp, "DATASET STRUCTURED_POINTS\n");
+  fprintf(fp, "DIMENSIONS %lu %lu %lu\n", Nx, Ny, Nz);
+  fprintf(fp, "ORIGIN %f %f %f\n", xmin, ymin, zmin);
+  fprintf(fp, "SPACING %f %f %f\n", dx, dy, dz);
+  fprintf(fp, "POINT_DATA %lu\n", Nx * Ny * Nz);
+  fprintf(fp, "VECTORS volume_vectors double\n");
+  for (const auto& data : result) {
+    const auto u = data.vel;
+    fprintf(fp, "%lf %lf %lf\n", u.x(), u.y(), u.z());
+  }
+  fclose(fp);
+  return result.size();
 }
 
 std::size_t RecordioWriter(const std::string& input_path,
-                    const std::string& output_path) {
+                           const std::string& output_path) {
   std::size_t index = 0;
   UVLM::proto::Snapshot2 snapshot;
   for (const auto& snapshot :
        recordio::ReaderRange<UVLM::proto::Snapshot2>(input_path)) {
     if (index == FLAGS_step) {
-      return Write(snapshot, OutputPath(output_path, index));
+      LOG(INFO) << "Step: " << FLAGS_step;
+      return Write(snapshot, output_path);
     }
     ++index;
   }
